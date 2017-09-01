1b06a33c0ac01b7b560724bbde07a609a4e65b08
shankar3 <bshankar@gleedtech.com>
2017-09-01 15:38:38 +0530

    changes for forms

--- src/app/client/client.component.ts
+++ src/app/client/client.component.ts
@@ -1,15 +1,29 @@
 import { Component, OnInit } from '@angular/core';
 
+
 @Component({
   selector: 'app-client',
   templateUrl: './client.component.html',
   styleUrls: ['./client.component.css']
 })
 export class ClientComponent implements OnInit {
-
+  
   constructor() { }
+  remGroup=[
+  ];
+  myModel={}
+  removeUser(i){
+    this.remGroup.splice(i, 1)
+  }
+    addUser(data){
+      this.remGroup.push(data);
+      this.myModel={}
+      // console.log("iiii");
+      // console.log(this.remGroup);
+
+    }
+    onSubmit(ngForm) {}
 
   ngOnInit() {
   }
-
 }
